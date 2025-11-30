4470e91cfdb69ef5032fa6f09d5697beba775de8
Shreyans Dharamchand <shreyansmaloo.1996@gmail.com>
2025-11-30 12:39:09 +0530

    Add CSP meta tag to upgrade insecure requests

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
